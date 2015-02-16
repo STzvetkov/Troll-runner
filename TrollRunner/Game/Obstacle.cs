f65629a9b642a62f09a2c960352349222933c1b4
Simeon Tzvetkov <simeon_tzvetkov@mail.bg>
2015-02-16 21:28:58 +0200

    Class diagram

---
+++ TrollRunner/Game/Obstacle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game
+{
+    public class Obstacle
+    {
+        public int Type
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
