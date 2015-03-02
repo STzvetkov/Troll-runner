1796b161e134cfee5b8a140147c1f005bb5b2ffb
BiserSirakov <biser.sirakov@gmail.com>
2015-03-02 12:23:33 +0200

    Obstacle

--- test/test/Obstacle.cs
+++ test/test/Obstacle.cs
@@ -16,9 +16,9 @@ namespace test
             this.Y = y;
         }
 
-        public int X { get; private set; }
+        public int X { get; protected set; }
 
-        public int Y { get; private set; }
+        public int Y { get; protected set; }
 
         protected virtual void PrintOnPosition(int obstacleRows, int obstacleCols)
         {
