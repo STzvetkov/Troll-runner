caa49d392c251c80fd107a8fbb630d1275feea8d
PetkoHa <darth_petko@abv.bg>
2015-03-02 12:18:22 +0200

    Added Runner and obsticle

--- test/test/Runner.cs
+++ test/test/Runner.cs
@@ -6,9 +6,44 @@ using System.Threading.Tasks;
 
 namespace test
 {
-    public class Runner
+    public class Runner : Obstacle
     {
-        public int X { get; set; }
-        public int Y { get; set; }
+
+        public char C { get; set; }
+
+        protected char[,] form;
+
+        public Runner(int x, int y, char c)
+            : base(x, y)
+        {
+
+            this.C = 'O';
+
+        }
+
+
+        private void FillLandObstacle()
+        {
+
+        }
+
+        protected virtual void PrintTrollOnPosition(int x, int y, char c)
+        {
+
+            Console.SetCursorPosition(this.X, this.Y);
+            Console.Write(c);
+
+        }
+
+
+        protected virtual void Jump()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                this.Y++;
+            }
+        }
     }
 }
+    
+
